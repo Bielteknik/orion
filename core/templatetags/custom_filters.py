bc380731d322ccd1769f8c337ae3799e34417e51
Kenan <48903906+Bielteknik@users.noreply.github.com>
2025-08-14 16:19:18 +0300

    filter update

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -1,5 +1,9 @@
 from django import template
+
 register = template.Library()
+
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(key)
+    if isinstance(dictionary, dict):
+        return dictionary.get(key)
+    return None
